02a1ef2fffec0b0eb999d59f43d8f60e6c667dec
sathwik <theweekend0006@gmail.com>
2025-10-09 15:24:51 +0530

    Initial Maven project commit

---
+++ src/main/java/com/app/demo/JAvaProject/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.JAvaProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
